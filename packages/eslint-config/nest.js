286846f1c81ddc96050a643edf528aa6baef5565
nucleus48 <nucleuserumagborie@gmail.com>
2025-12-28 18:51:04 +0100

    feat: Enhance ESLint Nest.js configuration with globals and parser options, and remove `moduleResolution: Bundler` from base tsconfig.

--- packages/eslint-config/nest.js
+++ packages/eslint-config/nest.js
@@ -1,5 +1,18 @@
 import turboConfig from "eslint-config-turbo/flat";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
 import baseConfigs from "./base.js";
 
-export default defineConfig(baseConfigs, turboConfig);
+export default defineConfig(baseConfigs, turboConfig, {
+  languageOptions: {
+    globals: {
+      ...globals.node,
+      ...globals.jest,
+    },
+    sourceType: "commonjs",
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: process.cwd(),
+    },
+  },
+});
