fc22801f9d70e52e3f315cb375307799f97fa038
nucleus48 <nucleuserumagborie@gmail.com>
2026-01-01 10:41:49 +0100

    chore: Add postcss.config.mjs to ESLint global ignores.

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -6,7 +6,7 @@ import tseslint from "typescript-eslint";
 
 export default defineConfig(
   gitignore({ cwd: process.cwd() }),
-  globalIgnores(["eslint.config.mjs"]),
+  globalIgnores(["eslint.config.mjs", "postcss.config.mjs"]),
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
